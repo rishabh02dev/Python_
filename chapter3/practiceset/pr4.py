ea9c5fa1c388c6d329c210ef4c049d7a040e996e
Rishabh Shukla <kumkumshukla631@gmail.com>
2022-12-27 18:48:05 +0530

    replaceproblem

--- chapter3/practiceset/pr4.py
+++ chapter3/practiceset/pr4.py
@@ -1,4 +1,4 @@
 name = "rishabh  shukla"
 name = name.replace("  " , " ")
 print(name)
-# and if there are two doubles space it will make it one 
+# and if there are two doubles space it will make it one  
