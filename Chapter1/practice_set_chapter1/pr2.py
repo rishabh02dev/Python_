6c2dc8c493f337d28aec83dce5b3bef3121b8e00
Rishabh Shukla <kumkumshukla631@gmail.com>
2022-12-27 18:35:55 +0530

    Using_Repl

--- Chapter1/practice_set_chapter1/pr2.py
+++ Chapter1/practice_set_chapter1/pr2.py
@@ -1,2 +1,2 @@
 #you can simply start by writing python in your windows powershell
-# and type python to intiaite repl that is read evaluate print loop
+# and type python to intiaite repl that is read evaluate print loop 
