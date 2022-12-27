02ce97d0b8a619d75380f3a4dcfedc6d98e8efff
Rishabh Shukla <kumkumshukla631@gmail.com>
2022-12-27 18:43:08 +0530

    Average_problem

--- Chapter2/practiceset/pr5.py
+++ Chapter2/practiceset/pr5.py
@@ -1,3 +1,3 @@
 num1 = int(input("Write the value of num1 "))
 num2 = int(input("Write the value of num2 "))
-print("the average of num1 and num2 is ", (num1+num2)/2)
+print("the average of num1 and num2 is ", (num1+num2)/2) 
