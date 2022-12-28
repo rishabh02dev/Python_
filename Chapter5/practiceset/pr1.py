0dede9c8488b8cc3fbe1f6469b7c9a3226a7743c
Rishabh Shukla <kumkumshukla631@gmail.com>
2022-12-28 22:01:19 +0530

    Update pr1.py

--- Chapter5/practiceset/pr1.py
+++ Chapter5/practiceset/pr1.py
@@ -2,7 +2,7 @@ hindi_to_english_dictionary = {
     "Balla" : "bat",
     "geeend" : "Ball",
     "Vastu " : "Item"
-}
+} 
 print("Options are", hindi_to_english_dictionary.keys())
 a = input("Enter the hindi Word\n")
 print("The meaning of your word is " , hindi_to_english_dictionary.get(a))
