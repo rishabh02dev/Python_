70d4ac67fde82bcab13d80fd9f5bf4a26898eab8
Rishabh Shukla <kumkumshukla631@gmail.com>
2023-01-02 20:30:13 +0530

    Example_during_oops

--- Chapter10/employee.py
+++ Chapter10/employee.py
@@ -13,6 +13,6 @@ Employee.company = "Twitter"
 print(harry.company)
 print(harry.salary)
 print(rajni.salary)
-
+ 
 
 #Instance attributes that belongs to the instances(object)partcular instance like rohit's adhaar number or his salary assuming the class from the previous example. and other is class attributes.
