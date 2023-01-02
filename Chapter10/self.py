7d7303e37ae7f31e3113d6901b3c4ef13f5859b0
Rishabh Shukla <kumkumshukla631@gmail.com>
2023-01-02 20:31:53 +0530

    What_is_Self

--- Chapter10/self.py
+++ Chapter10/self.py
@@ -2,7 +2,7 @@ class employee:
     company = "Google"
     def getsalary(self):
         print(f"Salary for this employee working in {self.company} is {self.salary}")
-
+ 
 
 harry = employee()
 harry.salary = 10000
